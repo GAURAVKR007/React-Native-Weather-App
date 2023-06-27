101840fcb5f38bdb5d2753786b98435e328d3f36
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-06-27 23:30:35 +0530

    Got location of person using Expo Location

--- App.jsx
+++ App.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
+import * as Location from 'expo-location';
 
 const App = () => {
+  // const [loading, setLoading] = useState(true);
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null) 
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setError('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  if(location) {
+    console.log(location);
+  }
+
+  // if(loading) {
+  //   return (
+  //     <View style={styles.container}>
+  //       <ActivityIndicator 
+  //       size={100}
+  //       color="#0000ff"
+  //       />
+  //     </View>
+  //   )
+  // }
+
+
   return (
     <NavigationContainer>
     <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    justifyContent : "center"
+  }
+})
 
 export default App;
