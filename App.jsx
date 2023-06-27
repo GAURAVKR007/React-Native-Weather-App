3f07baa4fa6aee10e7b9c13d6b5f2b8fd04dd207
Gaurav Kumar Thakur <gauravkrthakur07@gmail.com>
2023-06-27 23:58:34 +0530

    Added enivornment variables in project

--- App.jsx
+++ App.jsx
@@ -3,11 +3,15 @@ import { ActivityIndicator, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./src/components/Tabs";
 import * as Location from 'expo-location';
+import { TEST_KEY } from '@env';
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
   // const [loading, setLoading] = useState(true);
   const [location, setLocation] = useState(null);
   const [error, setError] = useState(null) 
+  console.log(TEST_KEY);
 
   useEffect(() => {
     (async () => {
@@ -24,7 +28,7 @@ const App = () => {
   }, []);
 
   if(location) {
-    console.log(location);
+    // console.log(location);
   }
 
   // if(loading) {
